b0cb1ea68af1bcaa93e022d3c3e36c89b141ff41
上田洸太 <uedakota@uedakoutanoMacBook-puro.local>
2019-07-19 14:27:25 +0900

    見た目の修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   with_options presence: true do
-  validates :title
-  validates :body
+    validates :title
+    validates :body
   end
  has_many :comments, :dependent => :destroy
 end
